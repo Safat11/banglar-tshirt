08a2d62efec840ce26110af6c284b35533a3357b
Safat11 <safat3622@gmail.com>
2024-08-31 01:35:01 +0600

    Remove From Cart And Not Allow Duplicate

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,12 +4,27 @@ import TShirt from '../TShirt/TShirt';
 import Cart from '../Cart/Cart';
 import './Home.css';
 import { useState } from 'react';
+import toast from 'react-hot-toast';
 
 const Home = () => {
-   const {cart, SetCart} = useState([]);
-   const handleAddToCart = tshirt => {
-    console.log(tshirt);
-   }
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt => {
+        const exists = cart.find(ts => ts._id === tshirt._id);
+        if (exists) {
+        toast("You have already added this t-shirt");
+        }
+        else {
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+        }
+
+    }
+
+    const handleRemoveFromCart = id => {
+        const remaining = cart.filter(ts => ts._id !== id);
+        setCart(remaining);
+    }
 
     const tshirts = useLoaderData();
     return (
@@ -24,7 +39,10 @@ const Home = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart
+                    cart={cart}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                ></Cart>
             </div>
         </div>
     );
